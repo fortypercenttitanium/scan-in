831cdf252c3939d55a3f34f5dd136277f267a88b
Alex Younger <alexanderyounger@gmail.com>
2021-08-17 18:05:04 -0400

    fix scripts and backup get route

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get('*', () => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
