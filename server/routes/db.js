a79ffd78fbe77c6c1c206b30158050d08687eb09
Alex Younger <alexanderyounger@gmail.com>
2021-08-28 10:12:08 -0400

    rename apollo server, add db route

---
+++ server/routes/db.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const passport = require('../auth/passport');
+const { gql } = require('graphql-request');
+const query = require('../helperFunctions/queryHelper');
+require('dotenv').config();
+
+router.use(passport.authenticate('jwt', { session: false }));
