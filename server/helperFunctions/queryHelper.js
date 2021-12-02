9de450e68ad869dc1c3d994dc3d27cad4001f6bf
Alex Younger <alexanderyounger@gmail.com>
2021-12-01 22:42:22 -0500

    attempt fix apollo server

--- server/helperFunctions/queryHelper.js
+++ server/helperFunctions/queryHelper.js
@@ -3,9 +3,11 @@ const { ApolloError } = require('apollo-server-express');
 
 const PORT = process.env.PORT || 5000;
 
+console.log('port ', PORT);
+
 async function query(q, variables) {
   try {
-    return await request(`http://localhost:${PORT}/graphql`, q, variables);
+    return await request(`/graphql`, q, variables);
   } catch (err) {
     throw new ApolloError(err);
   }
