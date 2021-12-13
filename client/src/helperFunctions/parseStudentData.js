3fbb10b7913069002c06dad727317c4df1d563b6
Alex Younger <alexanderyounger@gmail.com>
2021-12-13 14:06:32 -0500

    fix name validation

--- client/src/helperFunctions/parseStudentData.js
+++ client/src/helperFunctions/parseStudentData.js
@@ -1,14 +1,15 @@
 export default function parseStudentData(data) {
-  const regex = /^[A-Za-z-']+, [A-Za-z\-' .]+ \(\d{5,}\)$/;
+  const regex = /^[A-Za-z-' ]+, [A-Za-z\-' .]+ \(\d{5,}\)$/;
 
   if (!regex.test(data)) {
     return null;
   }
 
-  const split = data.split(' ').map((data) => data.trim());
-  const lastName = split[0].replace(',', '');
-  const firstName = split.slice(1, split.length - 1).join(' ');
-  const id = split[split.length - 1].replace(/\(|\)/g, '');
+  const split = data.split(',').map((data) => data.trim());
+  const lastName = split[0].trim();
+  const id = split[1].replace(/\(|\)/g, '');
+  const endFirstIndex = split[1].indexOf('(');
+  const firstName = split[1].slice(0, endFirstIndex).trim();
 
   return {
     firstName,
